3470f19ddcfe0592105157bb2ec910b6355385d3
Giordan Battaglin <gio@giordan.ca>
2015-11-19 16:40:42 -0500

    added stateprovider

--- js/app.esnext.js
+++ js/app.esnext.js
@@ -1,14 +1,38 @@
+//init app
 let app = angular.module('swillApp', ['ui.router']);
 
-let ACCESS_KEY = 'MDo3NTJjYzBmMC04ZWU2LTExZTUtYjkxYy04M2IwMzZlMmUwYTc6V1hLeXQ3cWRlYVFoRzFzZFF2NVdrM3JqTk9EN3l0aXRMc3d5';
+//config stateprovider
+app.config( ($stateProvider) => {
+	$stateProvider
+		.state('index', {
+			url: '',
+			controller: 'MainController',
+			templateUrl: 'js/templates/results.html'
+	})
+});
 
+//main controller
 app.controller('MainController', ['$scope', ($scope) => {
-
+		
 }]);
 
+//app.directive('singleSwill', () => {
+//	return {
+//		restrict: 'E',
+//		templateUrl: '/js'
+//	}
+//});
+
+//initial ajax call
 app.factory('products', ['$http', ($http) => {
+	let API_KEY = 'MDo3NTJjYzBmMC04ZWU2LTExZTUtYjkxYy04M2IwMzZlMmUwYTc6V1hLeXQ3cWRlYVFoRzFzZFF2NVdrM3JqTk9EN3l0aXRMc3d5';
+	let API_URL = 'http://lcboapi.com/products?access_key=';
+	let isDead = '&where_not=is_discontinued';
+	let perPage = '&per_page=200';
+	let swill = '&q=';
+	let endpoint = API_URL + API_KEY + isDead + perPage + swill;
 	return 
-			$http.get('http://lcboapi.com/products?access_key=' + API_KEY)
+			$http.get( endpoint )
 			.success((data) => {
 				return data;
 	})
--- js/app.js
+++ js/app.js
@@ -1,14 +1,37 @@
 'use strict';
 
+//init app
 var app = angular.module('swillApp', ['ui.router']);
 
-var ACCESS_KEY = 'MDo3NTJjYzBmMC04ZWU2LTExZTUtYjkxYy04M2IwMzZlMmUwYTc6V1hLeXQ3cWRlYVFoRzFzZFF2NVdrM3JqTk9EN3l0aXRMc3d5';
+//config stateprovider
+app.config(function ($stateProvider) {
+	$stateProvider.state('index', {
+		url: '',
+		controller: 'MainController',
+		templateUrl: 'js/templates/results.html'
+	});
+});
 
+//main controller
 app.controller('MainController', ['$scope', function ($scope) {}]);
 
+//app.directive('singleSwill', () => {
+//	return {
+//		restrict: 'E',
+//		templateUrl: '/js'
+//	}
+//});
+
+//initial ajax call
 app.factory('products', ['$http', function ($http) {
+	var API_KEY = 'MDo3NTJjYzBmMC04ZWU2LTExZTUtYjkxYy04M2IwMzZlMmUwYTc6V1hLeXQ3cWRlYVFoRzFzZFF2NVdrM3JqTk9EN3l0aXRMc3d5';
+	var API_URL = 'http://lcboapi.com/products?access_key=';
+	var isDead = '&where_not=is_discontinued';
+	var perPage = '&per_page=200';
+	var swill = '&q=';
+	var endpoint = API_URL + API_KEY + isDead + perPage + swill;
 	return;
-	$http.get('http://lcboapi.com/products?access_key=' + API_KEY).success(function (data) {
+	$http.get(endpoint).success(function (data) {
 		return data;
 	}).error(function (err) {
 		return err;
